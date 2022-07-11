6e2908a3ffa8d3e817fdbdf098c7e016c8922edf
Andreas Östling <andreas.ostling@ur.se>
2022-07-11 18:41:06 +0200

    update unit test for formatTime

--- src/components/MusicPlayer/helpers/helpers.test.ts
+++ src/components/MusicPlayer/helpers/helpers.test.ts
@@ -1,23 +1,20 @@
 import { formatTime } from "./helpers";
 
 describe("hej", () => {
-  test("should ...", () => {
+  test("handle 0 case", () => {
+    expect(formatTime(0)).toEqual("00:00");
+  });
+  test("should handle a minute", () => {
     expect(formatTime(60)).toEqual("01:00");
   });
-  test("should...", () => {
+  test("should handle minutes and seconds", () => {
     expect(formatTime(61)).toEqual("01:01");
   });
-  test("should...", () => {
+  test("should handle only seconds", () => {
     expect(formatTime(2)).toEqual("00:02");
   });
-  test("should...", () => {
-    expect(formatTime(3600)).toEqual("60:00");
-  });
-  test("should...", () => {
+  test("should handle large case of minutes with seconds", () => {
     expect(formatTime(3610)).toEqual("60:10");
-  });
-  test("should...", () => {
-    expect(formatTime(3610 * 2)).toEqual("120:20");
   });
 });
 
